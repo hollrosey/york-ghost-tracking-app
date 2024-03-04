863c746909a0bf63ece628345f4f8902c7b4448a
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-03-04 21:28:53 +0000

    empty functions created

--- functions.js
+++ functions.js
@@ -4,11 +4,26 @@ import { v4 as uuidv4 } from "uuid";
 const ghostFile = "ghosts.json";
 
 // Get all ghosts from ghosts.json
+export async function getGhosts(ghostFile) {
 
-// Get a ghost by ID 
+};
+
+// Get a ghost by ID (id in here is a placeholder- remember!)
+export async function getGhostByID(id) {
+
+};
 
 // Create a new ghost 
+export async function createGhost(newGhost, ghostFile) {
+
+};
 
 // Update a ghost by ID
+export async function updateGhostByID(id, updatedGhost, ghostFile) {
+
+};
 
 // Delete a ghost by ID
+export async function deleteGhostByID(id) {
+
+};
