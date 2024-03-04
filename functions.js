2d38897c2734ee332249c96abd40a0f38bfb3adb
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-03-04 21:52:44 +0000

    completed getGhosts

--- functions.js
+++ functions.js
@@ -5,45 +5,43 @@ const ghostFile = "ghosts.json";
 
 // Get all ghosts from ghosts.json
 export async function getGhosts(ghostFile) {
-    try {
-
-    } catch (error) {
-        console.error("Error reading file:", error)
-    }
-};
+  try {
+    const data = await fs.readFile(fileName, "utf-8");
+    const jsonData = JSON.parse(data);
+    return jsonData;
+  } catch (error) {
+    console.error("Error reading file:", error);
+  }
+}
 
 // Get a ghost by ID (id in here is a placeholder- remember!)
 export async function getGhostByID(id) {
-    try {
+  try {
+  } catch (error) {
+    console.error("Error reading file:", error);
+  }
+}
 
-    } catch (error) {
-        console.error("Error reading file:", error)
-    }
-};
-
-// Create a new ghost 
+// Create a new ghost
 export async function createGhost(newGhost, ghostFile) {
-    try {
-
-    } catch (error) {
-        console.error("Error creating ghost:", error)
-    }
-};
+  try {
+  } catch (error) {
+    console.error("Error creating ghost:", error);
+  }
+}
 
 // Update a ghost by ID
 export async function updateGhostByID(id, updatedGhost, ghostFile) {
-    try {
-
-    } catch (error) {
-        console.error("Error updating ghost:", error)
-    }
-};
+  try {
+  } catch (error) {
+    console.error("Error updating ghost:", error);
+  }
+}
 
 // Delete a ghost by ID
 export async function deleteGhostByID(id) {
-    try {
-
-    } catch (error) {
-        console.error("Error deleting ghost:", error)
-    }
-};
+  try {
+  } catch (error) {
+    console.error("Error deleting ghost:", error);
+  }
+}
