0a1bb92691640270f0ccf7650ba784ce7c96c80d
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-03-04 22:30:38 +0000

    populated update and delete functions

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ const ghostFile = "ghosts.json";
 // Get all ghosts from ghosts.json
 export async function getGhosts(ghostFile) {
   try {
-    const data = await fs.readFile(fileName, "utf-8");
+    const data = await fs.readFile(ghostFile, "utf-8");
     const jsonData = JSON.parse(data);
     return jsonData;
   } catch (error) {
@@ -17,7 +17,7 @@ export async function getGhosts(ghostFile) {
 // Get a ghost by ID
 export async function getGhostByID(id) {
   try {
-    const data = await fs.readFile(fileName, "utf-8");
+    const data = await fs.readFile(ghostFile, "utf-8");
     const jsonData = JSON.parse(data);
     const find = jsonData.find(({ id }) => id === id);
     if (find) {
@@ -33,7 +33,7 @@ export async function getGhostByID(id) {
 // Create a new ghost
 export async function createGhost(newGhost, ghostFile) {
   try {
-    const data = await fs.readFile(fileName, "utf-8");
+    const data = await fs.readFile(ghostFile, "utf-8");
 
     const jsonData = JSON.parse(data);
 
@@ -53,6 +53,21 @@ export async function createGhost(newGhost, ghostFile) {
 // Update a ghost by ID
 export async function updateGhostByID(id, updatedGhost, ghostFile) {
   try {
+    const data = await fs.readFile(ghostFile, "utf-8");
+
+    const jsonData = JSON.parse(data);
+
+    const index = jsonData.findIndex((ghost) => ghost.id === id);
+
+    if (index !== -1) {
+        jsonData[index] = { ...jsonData[index], ...updatedGhost };
+
+        await fs.writeFile(ghostFile, JSON.stringify(jsonData, null, 2));
+
+        return jsonData[index];
+    } else {
+        throw new Error(`Ghost with ID ${id} not found.`);
+    }
   } catch (error) {
     console.error("Error updating ghost:", error);
   }
@@ -61,6 +76,21 @@ export async function updateGhostByID(id, updatedGhost, ghostFile) {
 // Delete a ghost by ID
 export async function deleteGhostByID(id) {
   try {
+    const data = await fs.readFile(ghostFile, "utf-8");
+
+    const jsonData = JSON.parse(data);
+
+    const index = jsonData.findIndex((ghost) => ghost.id === id);
+
+    if (index !== -1) {
+        const deletedGhost = jsonData.splice(index, 1)[0];
+
+        await fs.writeFile(ghostFile, JSON.stringify(jsonData, null, 2));
+
+        return deletedGhost;
+    } else {
+        throw new Error(`Ghost with ID ${id} not found.`);
+    }
   } catch (error) {
     console.error("Error deleting ghost:", error);
   }
