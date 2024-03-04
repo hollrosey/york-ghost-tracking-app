4bf7ab0b7391bb007293f60853c3d5b3ffeffb1f
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-03-04 21:51:42 +0000

    added try and catch to functions

--- functions.js
+++ functions.js
@@ -5,25 +5,45 @@ const ghostFile = "ghosts.json";
 
 // Get all ghosts from ghosts.json
 export async function getGhosts(ghostFile) {
+    try {
 
+    } catch (error) {
+        console.error("Error reading file:", error)
+    }
 };
 
 // Get a ghost by ID (id in here is a placeholder- remember!)
 export async function getGhostByID(id) {
+    try {
 
+    } catch (error) {
+        console.error("Error reading file:", error)
+    }
 };
 
 // Create a new ghost 
 export async function createGhost(newGhost, ghostFile) {
+    try {
 
+    } catch (error) {
+        console.error("Error creating ghost:", error)
+    }
 };
 
 // Update a ghost by ID
 export async function updateGhostByID(id, updatedGhost, ghostFile) {
+    try {
 
+    } catch (error) {
+        console.error("Error updating ghost:", error)
+    }
 };
 
 // Delete a ghost by ID
 export async function deleteGhostByID(id) {
+    try {
 
+    } catch (error) {
+        console.error("Error deleting ghost:", error)
+    }
 };
